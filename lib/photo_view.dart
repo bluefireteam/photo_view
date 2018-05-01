cb98747eeda01420bf49f62f3f69beb8fff9f24d
Renan C. Araújo <renancaraujo@outlook.com>
2018-04-30 22:55:32 -0300

    future

--- lib/photo_view.dart
+++ lib/photo_view.dart
@@ -57,6 +57,7 @@ class _PhotoViewState extends State<PhotoView>{
   @override
   Widget build(BuildContext context) {
     return new FutureBuilder(
+        future: _getImage(),
         builder: (BuildContext context, AsyncSnapshot<ImageInfo> info) {
           if(info.hasData){
             return new PhotoViewImageWrapper(
