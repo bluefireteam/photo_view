6dbbf40f5ba0e5a9369f8462c2b73daad60f2879
Renan C. Araujo <renancaraujo@outlook.com>
2018-08-18 17:48:48 -0300

    dartdocs

--- lib/photo_view.dart
+++ lib/photo_view.dart
@@ -10,16 +10,77 @@ import 'package:after_layout/after_layout.dart';
 
 export 'package:photo_view/photo_view_scale_boundary.dart';
 
-class PhotoView extends StatefulWidget{
-  final ImageProvider imageProvider;
-  final Widget loadingChild;
-  final Color backgroundColor;
-  final dynamic minScale;
-  final dynamic maxScale;
-  final bool gaplessPlayback;
-  final Size size;
-  final Object heroTag;
 
+/// A [StatefulWidget] that contains all the photo view rendering elements.
+///
+/// Internally, the image is rendered within an [Image] widget.
+///
+/// To use along a hero animation, provide [heroTag] param.
+///
+/// Sample code:
+///
+/// ```
+/// PhotoView(
+///  imageProvider: imageProvider,
+///  loadingChild: new LoadingText(),
+///  backgroundColor: Colors.white,
+///  minScale: PhotoViewScaleBoundary.contained,
+///  maxScale: 2.0,
+///  gaplessPlayback: false,
+///  size:MediaQuery.of(context).size,
+///  heroTag: "someTag"
+/// );
+/// ```
+///
+
+class PhotoView extends StatefulWidget{
+
+  /// Creates a widget that displays an zoomable image.
+  ///
+  /// To show an image from the network or from an asset bundle, use their respective
+  /// image providers, ie: [AssetImage] or [NetworkImage]
+  ///
+  /// The [maxScale] and [minScale] arguments may be [double] or a [PhotoViewScaleBoundary] constant
+  ///
+  /// Sample using [maxScale] and [minScale]
+  ///
+  /// ```
+  /// PhotoView(
+  ///  imageProvider: imageProvider,
+  ///  minScale: PhotoViewScaleBoundary.contained * 1.8,
+  ///  maxScale: PhotoViewScaleBoundary.covered * 1.1
+  /// );
+  /// ```
+  /// [size] is used to define the viewPort size in which the image will be
+  /// scaled to. This argument is rarely used. By befault is the size of the
+  /// screen. [PhotoViewInline] defines is as the size of the widget.
+  ///
+  /// The argument [gaplessPlayback] is used to continue showing the old image
+  /// (`true`), or briefly show nothing (`false`), when the [imageProvider]
+  /// changes.By default it's set to `false`.
+  ///
+  /// To use within an hero animation, specify [heroTag]. When [heroTag] is
+  /// specified, the image provider retrieval process should be sync.
+  ///
+  /// Sample using hero animation
+  /// ```
+  /// // screen1
+  ///   ...
+  ///   Hero(
+  ///     tag: "someTag",
+  ///     child: Image.asset(
+  ///       "assets/large-image.jpg",
+  ///       width: 150.0
+  ///     ),
+  ///   )
+  /// // screen2
+  /// ...
+  /// child: PhotoView(
+  ///   imageProvider: AssetImage("assets/large-image.jpg"),
+  ///   heroTag: "someTag",
+  /// )
+  /// ```
+  ///
   const PhotoView({
     Key key,
     @required this.imageProvider,
@@ -31,6 +92,40 @@ class PhotoView extends StatefulWidget{
     this.size,
     this.heroTag,
   }) : super(key: key);
+
+  /// Given a [imageProvider] it resolves into an zoomable image widget using. It
+  /// is required
+  final ImageProvider imageProvider;
+
+  /// While [imageProvider] is not resolved, [loadingChild] is build by [PhotoView]
+  /// into the screen, by default it is a centered [CircularProgressIndicator]
+  final Widget loadingChild;
+
+  /// Changes the background behind image, defaults to `Colors.black`.
+  final Color backgroundColor;
+
+  /// Defines the minimal size in which the image will be allowed to assume, it
+  /// is proportional to the original image size. Can be either a double or a
+  /// [PhotoViewScaleBoundary]
+  final dynamic minScale;
+
+  /// Defines the maximal size in which the image will be allowed to assume, it
+  /// is proportional to the original image size. Can be either a double or a
+  /// [PhotoViewScaleBoundary]
+  final dynamic maxScale;
+
+  /// This is used to continue showing the old image (`true`), or briefly show
+  /// nothing (`false`), when the `imageProvider` changes. By default it's set
+  /// to `false`.
+  final bool gaplessPlayback;
+
+  /// Defines the size of the scaling base of the image inside [PhotoView],
+  /// by default it is `MediaQuery.of(context).size`. This argument is used by
+  /// [PhotoViewInline] class.
+  final Size size;
+
+  /// Assists the activation of a hero animation within [PhotoView]
+  final Object heroTag;
 
   @override
   State<StatefulWidget> createState() {
@@ -136,7 +231,8 @@ class _PhotoViewState extends State<PhotoView>{
   }
 }
 
-
+/// A [StatelessWidget] which the only child is a [PhotoView] with an automacally
+/// calculated [size]. All but [size] arguments are the same as [PhotoView].
 class PhotoViewInline extends StatefulWidget{
   final ImageProvider imageProvider;
   final Widget loadingChild;
