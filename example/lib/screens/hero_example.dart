865742cda5cc54678fa2992e4a602037386b1f96
Renan <renan.araujo@dextra-sw.com>
2018-10-06 14:59:05 -0300

    flutter format

--- example/lib/screens/hero_example.dart
+++ example/lib/screens/hero_example.dart
@@ -19,12 +19,12 @@ class HeroExample extends StatelessWidget {
             child: GestureDetector(
               onTap: () {
                 Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => const HeroPhotoViewWrapper(
-                          imageProvider: AssetImage("assets/large-image.jpg"),
-                        ),
-                  ));
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const HeroPhotoViewWrapper(
+                            imageProvider: AssetImage("assets/large-image.jpg"),
+                          ),
+                    ));
               },
               child: Container(
                   child: Hero(
@@ -46,27 +46,26 @@ class HeroPhotoViewWrapper extends StatelessWidget {
   final dynamic minScale;
   final dynamic maxScale;
 
-  const HeroPhotoViewWrapper({
-    this.imageProvider,
-    this.loadingChild,
-    this.backgroundColor,
-    this.minScale,
-    this.maxScale
-  });
+  const HeroPhotoViewWrapper(
+      {this.imageProvider,
+      this.loadingChild,
+      this.backgroundColor,
+      this.minScale,
+      this.maxScale});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      constraints: BoxConstraints.expand(
-        height: MediaQuery.of(context).size.height,
-      ),
-      child: PhotoView(
-        imageProvider: imageProvider,
-        loadingChild: loadingChild,
-        backgroundColor: backgroundColor,
-        minScale: minScale,
-        maxScale: maxScale,
-        heroTag: "someTag",
-      ));
+        constraints: BoxConstraints.expand(
+          height: MediaQuery.of(context).size.height,
+        ),
+        child: PhotoView(
+          imageProvider: imageProvider,
+          loadingChild: loadingChild,
+          backgroundColor: backgroundColor,
+          minScale: minScale,
+          maxScale: maxScale,
+          heroTag: "someTag",
+        ));
   }
 }
