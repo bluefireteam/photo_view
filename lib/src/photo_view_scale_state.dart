97075740168817701e4a0931703889cb1f598e04
Carlit0 <carlostrachwitz@googlemail.com>
2019-04-13 16:19:30 +0200

    formatted Enum

--- lib/src/photo_view_scale_state.dart
+++ lib/src/photo_view_scale_state.dart
@@ -1,2 +1,9 @@
 /// A way to represent the step of the "doubletap gesture cycle" in which PhotoView is.
-enum PhotoViewScaleState { initial, covering, originalSize, zooming, zoomedIn, zoomedOut} // 'zooming' will get unnecessary
+enum PhotoViewScaleState {
+  initial,
+  covering,
+  originalSize,
+  zooming,
+  zoomedIn,
+  zoomedOut
+} // 'zooming' will get unnecessary
