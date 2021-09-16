d4466155c1273b876ca6f8a361669eb47faefccc
renancaraujo <renan.araujo@dashdash.com>
2021-09-16 21:56:39 +0100

    Fix(#464) network image

--- lib/src/core/photo_view_core.dart
+++ lib/src/core/photo_view_core.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/widgets.dart';
-
 import 'package:photo_view/photo_view.dart'
     show
         PhotoViewScaleState,
@@ -11,9 +10,9 @@ import 'package:photo_view/photo_view.dart'
 import 'package:photo_view/src/controller/photo_view_controller.dart';
 import 'package:photo_view/src/controller/photo_view_controller_delegate.dart';
 import 'package:photo_view/src/controller/photo_view_scalestate_controller.dart';
-import 'package:photo_view/src/utils/photo_view_utils.dart';
 import 'package:photo_view/src/core/photo_view_gesture_detector.dart';
 import 'package:photo_view/src/core/photo_view_hit_corners.dart';
+import 'package:photo_view/src/utils/photo_view_utils.dart';
 
 const _defaultDecoration = const BoxDecoration(
   color: const Color.fromRGBO(0, 0, 0, 1.0),
