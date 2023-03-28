b1974584a1eb318ef441c2bbffd61c0b42a0aa0b
Timestamp <60820399+TimestampWombat@users.noreply.github.com>
2023-03-28 20:37:23 +0800

    make scale more smoothly (#450)
    According to GestureDector.dragStartBehavior description between
    DragStartBehavior.start and DragStartBehavior.down, to make image
    scalable more smoothly, we should set PhotoViewGestureRecognizer
    dragStartBehavior to DragStartBehavior.start.
    
    Co-authored-by: FaKenKoala <60820399+FaKenKoala@users.noreply.github.com>

--- lib/src/core/photo_view_gesture_detector.dart
+++ lib/src/core/photo_view_gesture_detector.dart
@@ -66,6 +66,7 @@ class PhotoViewGestureDetector extends StatelessWidget {
           hitDetector: hitDetector, debugOwner: this, validateAxis: axis),
       (PhotoViewGestureRecognizer instance) {
         instance
+          ..dragStartBehavior = DragStartBehavior.start
           ..onStart = onScaleStart
           ..onUpdate = onScaleUpdate
           ..onEnd = onScaleEnd;
